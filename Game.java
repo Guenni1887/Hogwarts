3b3105707c5c4d81f65275643dd95c45b609e4f8
Guenni1887 <finn.unger.mail@gmail.com>
2022-11-15 08:06:56 +0000

    Löschung unnötiger Import

--- Game.java
+++ Game.java
@@ -1,5 +1,3 @@
-import javax.management.Descriptor;
-
 /**
  *  This class is the main class of the "World of Zuul" application. 
  *  "World of Zuul" is a very simple, text based adventure game.  Users 
