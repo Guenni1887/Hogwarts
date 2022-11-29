966dabb9c07bbaf345f781941f7d622a369aa335
Guenni1887 <finn.unger.mail@gmail.com>
2022-11-29 08:05:30 +0000

    refactor Exits

--- Game.java
+++ Game.java
@@ -1,22 +1,21 @@
 /**
- *  This class is the main class of the "World of Zuul" application. 
- *  "World of Zuul" is a very simple, text based adventure game.  Users 
- *  can walk around some scenery. That's all. It should really be extended 
- *  to make it more interesting!
+ * This class is the main class of the "World of Zuul" application.
+ * "World of Zuul" is a very simple, text based adventure game. Users
+ * can walk around some scenery. That's all. It should really be extended
+ * to make it more interesting!
  * 
- *  To play this game, create an instance of this class and call the "play"
- *  method.
+ * To play this game, create an instance of this class and call the "play"
+ * method.
  * 
- *  This main class creates and initialises all the others: it creates all
- *  rooms, creates the parser and starts the game.  It also evaluates and
- *  executes the commands that the parser returns.
+ * This main class creates and initialises all the others: it creates all
+ * rooms, creates the parser and starts the game. It also evaluates and
+ * executes the commands that the parser returns.
  * 
- * @author  Michael Kolling and David J. Barnes
+ * @author Michael Kolling and David J. Barnes
  * @version 1.0 (February 2002)
  */
 
-class Game 
-{
+class Game {
     // ~~~ initialization ~~~
     public static void main(String args[]) {
         Game g = new Game();
@@ -26,12 +25,11 @@ class Game
     private Parser parser;
     private Room currentRoom;
     private Player player;
-        
+
     /**
      * Create the game and initialise its internal map.
      */
-    public Game() 
-    {
+    public Game() {
         createRooms();
         parser = new Parser();
         player = new Player(10);
@@ -40,84 +38,83 @@ class Game
     /**
      * Create all the rooms and link their exits together.
      */
-    private void createRooms()
-    {
-        Room Bellcourt, HagridsHut, GreatHall,TrophyRoom, Staircase, Basement, SlytherinCommonRoom, PotionsClassRoom, FirstFloor, SecondFloor, Libary, InnerCourtyard, 
-        TransformationClassRoom, ThirdFloor, FourthFloor, FifthFloor, SixthFloor, SeventhFloor;
+    private void createRooms() {
+        Room Bellcourt, HagridsHut, GreatHall, TrophyRoom, Staircase, Basement, SlytherinCommonRoom, PotionsClassRoom,
+                FirstFloor, SecondFloor, Libary, InnerCourtyard,
+                TransformationClassRoom, ThirdFloor, FourthFloor, FifthFloor, SixthFloor, SeventhFloor;
         // create the rooms
         Bellcourt = new Room("in the courtyard in front of the bell tower");
         HagridsHut = new Room("in front of Hagrids hut");
         GreatHall = new Room("in the great hall");
         TrophyRoom = new Room("in the trophy room of Hogwarts");
-        Staircase = new Room ("in the only Staircase in the world where the stairs are moving");
+        Staircase = new Room("in the only Staircase in the world where the stairs are moving");
         Basement = new Room("in the basement");
-        SlytherinCommonRoom = new Room ("in front of the Slytherin common room. You need a password to enter");
-        PotionsClassRoom = new Room ("in the class room for the subject Potions");
-        FirstFloor = new Room ("in the first floor of the staircase");
-        SecondFloor = new Room ("in the second floor of the staircase");
+        SlytherinCommonRoom = new Room("in front of the Slytherin common room. You need a password to enter");
+        PotionsClassRoom = new Room("in the class room for the subject Potions");
+        FirstFloor = new Room("in the first floor of the staircase");
+        SecondFloor = new Room("in the second floor of the staircase");
         Libary = new Room("in the Libary of Hogwarts");
         InnerCourtyard = new Room("in the inner courtyard");
-        TransformationClassRoom =new Room("in the class room for the subject transformation");
+        TransformationClassRoom = new Room("in the class room for the subject transformation");
         ThirdFloor = new Room("in the third floor of the staircase");
-        FourthFloor =new Room("in the fourth floor of the staircase");
-        FifthFloor= new Room("in the fifth floor of the staircase");
-        SixthFloor= new Room("in the sixth floor of the staircase");
-        SeventhFloor= new Room( "in the seventh floor of the staircase");
- 
+        FourthFloor = new Room("in the fourth floor of the staircase");
+        FifthFloor = new Room("in the fifth floor of the staircase");
+        SixthFloor = new Room("in the sixth floor of the staircase");
+        SeventhFloor = new Room("in the seventh floor of the staircase");
+
         // initialise room exits
-        //outside
-        Bellcourt.setExits(GreatHall, null,  HagridsHut, null);
+        // outside
+        Bellcourt.setExits(GreatHall, null, HagridsHut, null);
         HagridsHut.setExits(Bellcourt, null, null, null);
 
-        //inside
+        // inside
         GreatHall.setExits(TrophyRoom, null, Bellcourt, Staircase);
         TrophyRoom.setExits(null, null, GreatHall, null);
-        Staircase.setExits(FirstFloor, GreatHall, null,Basement);
+        Staircase.setExits(FirstFloor, GreatHall, null, Basement);
 
-        //basement
-        Basement.setExits(SlytherinCommonRoom, Staircase, null, PotionsClassRoom);  
-        SlytherinCommonRoom.setExits(null, null, Basement,null);
+        // basement
+        Basement.setExits(SlytherinCommonRoom, Staircase, null, PotionsClassRoom);
+        SlytherinCommonRoom.setExits(null, null, Basement, null);
         PotionsClassRoom.setExits(null, Basement, null, null);
-        //first floor
+        // first floor
 
         FirstFloor.setExits(SecondFloor, null, Staircase, null);
 
-        //second floor
+        // second floor
         SecondFloor.setExits(ThirdFloor, InnerCourtyard, FirstFloor, Libary);
         Libary.setExits(null, SecondFloor, null, null);
         InnerCourtyard.setExits(null, TransformationClassRoom, null, SecondFloor);
         TransformationClassRoom.setExits(null, null, null, InnerCourtyard);
 
-        //third floor
+        // third floor
         ThirdFloor.setExits(FourthFloor, null, SecondFloor, null);
 
-        //fourth floor
+        // fourth floor
         FourthFloor.setExits(FifthFloor, null, ThirdFloor, null);
 
-        //fifth floor
+        // fifth floor
         FifthFloor.setExits(SixthFloor, null, FourthFloor, null);
 
-        //sixth floor
+        // sixth floor
         SixthFloor.setExits(SeventhFloor, null, FifthFloor, null);
 
-        //seventh floor
-        SeventhFloor.setExits(null, null, SixthFloor,null);
+        // seventh floor
+        SeventhFloor.setExits(null, null, SixthFloor, null);
 
-        currentRoom = Bellcourt;  // start game outside
+        currentRoom = Bellcourt; // start game outside
     }
 
     /**
-     *  Main play routine.  Loops until end of play.
+     * Main play routine. Loops until end of play.
      */
-    public void play() 
-    {            
+    public void play() {
         printWelcome();
 
-        // Enter the main command loop.  Here we repeatedly read commands and
+        // Enter the main command loop. Here we repeatedly read commands and
         // execute them until the game is over.
-                
+
         boolean finished = false;
-        while (! finished) {
+        while (!finished) {
             Command command = parser.getCommand();
             finished = processCommand(command);
         }
@@ -127,22 +124,27 @@ class Game
     /**
      * Print out the opening message for the player.
      */
-    private void printWelcome()
-    {
+    private void printWelcome() {
         System.out.println();
         System.out.println("Welcome to Hogwarts");
-        System.out.println("The other first years got thier breifing. If you dont have it, you need to find your way on your own");
+        System.out.println(
+                "The other first years got thier breifing. If you dont have it, you need to find your way on your own");
         System.out.println("Type 'help' if you need help for that.");
         System.out.println();
+        roomInfo();
+
+    }
+
+    private void roomInfo() {
         System.out.println("You are " + currentRoom.getDescription());
         System.out.print("Exits: ");
-        if(currentRoom.northExit != null)
+        if (currentRoom.northExit != null)
             System.out.print("north ");
-        if(currentRoom.eastExit != null)
+        if (currentRoom.eastExit != null)
             System.out.print("east ");
-        if(currentRoom.southExit != null)
+        if (currentRoom.southExit != null)
             System.out.print("south ");
-        if(currentRoom.westExit != null)
+        if (currentRoom.westExit != null)
             System.out.print("west ");
         System.out.println();
     }
@@ -152,11 +154,10 @@ class Game
      * If this command ends the game, true is returned, otherwise false is
      * returned.
      */
-    private boolean processCommand(Command command) 
-    {
+    private boolean processCommand(Command command) {
         boolean wantToQuit = false;
 
-        if(command.isUnknown()) {
+        if (command.isUnknown()) {
             System.out.println("I don't know what you mean...");
             return false;
         }
@@ -176,26 +177,24 @@ class Game
 
     /**
      * Print out some help information.
-     * Here we print some stupid, cryptic message and a list of the 
+     * Here we print some stupid, cryptic message and a list of the
      * command words.
      */
-    private void printHelp() 
-    {
+    private void printHelp() {
         System.out.println("You are lost. You are alone. You wander");
         System.out.println("around at the university.");
-        System.out.println("learning progress "+ player.getLearn());
+        System.out.println("learning progress " + player.getLearn());
         System.out.println();
         System.out.println("Your command words are:");
         System.out.println("   go quit help");
     }
 
-    /** 
+    /**
      * Try to go to one direction. If there is an exit, enter
      * the new room, otherwise print an error message.
      */
-    private void goRoom(Command command) 
-    {
-        if(!command.hasSecondWord()) {
+    private void goRoom(Command command) {
+        if (!command.hasSecondWord()) {
             // if there is no second word, we don't know where to go...
             System.out.println("Go where?");
             return;
@@ -204,46 +203,26 @@ class Game
         String direction = command.getSecondWord();
 
         // Try to leave current room.
-        Room nextRoom = null;
-        if(direction.equals("north"))
-            nextRoom = currentRoom.northExit;
-        if(direction.equals("east"))
-            nextRoom = currentRoom.eastExit;
-        if(direction.equals("south"))
-            nextRoom = currentRoom.southExit;
-        if(direction.equals("west"))
-            nextRoom = currentRoom.westExit;
-
+        Room nextRoom = currentRoom.getExit(direction);
+       
         if (nextRoom == null)
             System.out.println("There is no door!");
         else {
             currentRoom = nextRoom;
-            System.out.println("You are " + currentRoom.getDescription());
-            System.out.print("Exits: ");
-            if(currentRoom.northExit != null)
-                System.out.print("north ");
-            if(currentRoom.eastExit != null)
-                System.out.print("east ");
-            if(currentRoom.southExit != null)
-                System.out.print("south ");
-            if(currentRoom.westExit != null)
-                System.out.print("west ");
-            System.out.println();
+            roomInfo();
         }
     }
 
-    /** 
+    /**
      * "Quit" was entered. Check the rest of the command to see
      * whether we really quit the game. Return true, if this command
      * quits the game, false otherwise.
      */
-    private boolean quit(Command command) 
-    {
-        if(command.hasSecondWord()) {
+    private boolean quit(Command command) {
+        if (command.hasSecondWord()) {
             System.out.println("Quit what?");
             return false;
-        }
-        else
-            return true;  // signal that we want to quit
+        } else
+            return true; // signal that we want to quit
     }
 }
--- Room.java
+++ Room.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /*
  * Class Room - a room in an adventure game.
  *
@@ -16,10 +18,7 @@
 class Room 
 {
     public String description;
-    public Room northExit;
-    public Room southExit;
-    public Room eastExit;
-    public Room westExit;
+    public HashMap<String, Room>Exits;
 
     /**
      * Create a room described "description". Initially, it has
@@ -29,31 +28,28 @@ class Room
     public Room(String description) 
     {
         this.description = description;
+        Exits = new HashMap<String, Room>();
     }
 
     /**
      * Define the exits of this room.  Every direction either leads
      * to another room or is null (no exit there).
      */
-    public void setExits(Room north, Room east, Room south, Room west) 
+    public void setExit(String direction, Room nextRoom) 
     {
-        if(north != null)
-            northExit = north;
-        if(east != null)
-            eastExit = east;
-        if(south != null)
-            southExit = south;
-        if(west != null)
-            westExit = west;
+       Exits.put(direction, nextRoom);
     }
 
     /**
      * Return the description of the room (the one that was defined
      * in the constructor).
      */
-    public String getDescription()
+   public String getDescription()
     {
         return description;
+    }
+    public Room getExit(String direction){
+        return (Room) Exits.get(direction);
     }
 
 }
